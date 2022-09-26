2f322be15f5a58feb78101146b412e12d3e8ae73
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-09-26 06:54:36 +0000

    a few changes to input of A

--- .ccls-cache/@home@runner@HW-5-problem-4/main.cpp
+++ .ccls-cache/@home@runner@HW-5-problem-4/main.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 int main() {
 //In sample input , the numbers modulo 42 are, 1,2,3,4,5,6,7,8,9,10
-  
-  for (int i = 0; i <= 10; ++i){
+  int A, B;
+  //cin >> A >> B;
+  for (int i = 1; i <= 10; ++i){
   int A = 0;
   int B = 42;
-  //int answer;
+  int answer;
      //answer = i % B;
-    if ( i != 1){
+      answer = i % B;
+    
+      //cout << answer << endl;
+    if (A % B == i){
       continue;
     }
-    cout << (A % B) << endl;
+    cout << answer;
+      }
+    
+    //cout << answer << endl;
     
     
-    }
+    
   return 0;
   
-}
+  }
+  
