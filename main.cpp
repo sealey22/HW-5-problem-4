6176d356f2e7644156a9e771f9d206cb8176e3dd
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-09-26 06:34:42 +0000

    Sample inputs trying to understand wording of problem

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main() {
+//In sample input , the numbers modulo 42 are, 1,2,3,4,5,6,7,8,9,10
   
+  for (int i = 0; i <= 10; ++i){
+  int A = 0;
+  int B = 42;
+  //int answer;
+     //answer = i % B;
+    if ( i != 1){
+      continue;
+    }
+    cout << (A % B) << endl;
+    
+    
+    }
+  return 0;
   
 }
